7bb3b3ca8937e9e4a49c0c29b399f47815383f33
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-06-19 11:23:49 +0900

    feat: add Link Link to addCardPage

--- src/pages/cardList/index.tsx
+++ src/pages/cardList/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const CardList = () => {
-  return <h1>CardList</h1>;
+  return (
+    <>
+      <h1>CardList</h1>
+      <Link to="/add">addCardPage</Link>
+    </>
+  );
 };
